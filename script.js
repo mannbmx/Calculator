d05369e493d4832a97fa2802786614cd4d1b6c1c
mannbmx <mannbmx@gmail.com>
2022-03-17 19:43:50 -0400

    Started number in put and addition

--- script.js
+++ script.js
@@ -7,11 +7,46 @@ const prevAns = document.querySelector('#prevAns');
 prevAns.textContent = secondNumber;
 
 const numButton = document.querySelectorAll('.number');
-
+let a;
+let b = '';
 for(let i = 0; i < numButton.length; i++) {
-    numButton[i].addEventListener('click', function(e) {console.log(e.target.value)});
+   
+    numButton[i].addEventListener('click', function(event) {
+        a = event.target.value;
+        a = a.toString();
+        b += a;
+        primaryNum.textContent = b;
+        console.log(a);
+    });
 }
 
-for(let i = 0; i < 10; i++) {
-    
-}
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    primaryNum.textContent = 0;
+    firstNumber = 0;
+    secondNumber = 0;
+});
+
+const equal = document.querySelector('#equal');
+const add = document.querySelector('#addition');
+add.addEventListener('click', ()=> {
+    firstNumber = parseFloat(b);
+    primaryNum.textContent = 0;
+
+    for(let i = 0; i < numButton.length; i++) {
+   
+        numButton[i].addEventListener('click', function(event) {
+            let c = event.target.value;
+            c = c.toString();
+            let d = '';
+            d += c;
+            primaryNum.textContent = b;
+            secondNumber = parseFloat(d);
+        });
+    }
+
+    equal.addEventListener('click', () =>{
+        primaryNum.textContent = firstNumber + secondNumber;
+    })
+});
+
